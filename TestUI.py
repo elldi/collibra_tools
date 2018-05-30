9ee62704b1dab90ec2285404d51b7e5157681317
elldi <elliotdines1@gmail.com>
2018-05-30 17:29:21 +0100

    Start of Tkinter user interface, converting dev into a OO design. Will likely move out to its own file.

---
+++ TestUI.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+Imports
+"""
+import sys
+import argparse
+import requests
+import json
+import re
+import shlex
+from tkinter import *
+from tkinter import ttk
+
+class WorkflowTestUI:
+
+    def __init__(self):
+        ## Set up of UI.
+        root = Tk()
+        root.title("Workflow Testing Tool")
+
+
+        
+
+#-----------
+# Main
+#-----------
+
+testUI = WorkflowTestUI()
+
+
+        
--- dev.py
+++ dev.py
@@ -10,42 +10,48 @@ import json
 import re
 import shlex
 
+
+class DataManip:
+    
+    def __init__(self, baseURL):
+        self.baseURL = baseURL
+        
+    def createDomain(self):
+        print("Domain Created")
+
+    def findDomainType(self):
+        print("Domain Type Found!")
+
+    def createAsset(self,assetName, domainId, assetTypeId):
+        
+        headers = {'Content-Type' : 'application/json'}
+        payload = {"name": assetName,"domainId": domainId,"typeId": assetTypeId}
+        payload = json.dumps(payload)
+        r = requests.post(self.baseURL + '/rest/2.0/assets',headers=headers, data=payload, auth=('Admin','admin'))
+        print r.json()        
+        return True
+
+    def findAssetType(self,assetTypeName):
+        try:
+            r = requests.get(self.baseURL + '/rest/2.0/assetTypes?name='+ assetTypeName + '&nameMatchMode=ANYWHERE', auth=('Admin', 'admin'))
+            print r.json()['total']
+            if(r.json()['total'] > 0):
+               return r.json()['results'][0]['id']
+            return ''
+        except ValueError:
+            return False
+
 #-----------
 # Functions
 #-----------
-
-    
-
-def createDomain():
-    print("Domain Created")
-
-def findDomainType():
-    print("Domain Type Found!")
-
-def createAsset(assetName, domainId, assetTypeId):
-    
-    headers = {'Content-Type' : 'application/json'}
-    payload = {"name": assetName,"domainId": domainId,"typeId": assetTypeId}
-    payload = json.dumps(payload)
-    r = requests.post('http://localhost:4400/rest/2.0/assets',headers=headers, data=payload, auth=('Admin','admin'))
-    print r.json()        
-    return True
-
-def findAssetType(assetTypeName):
-    try:
-        r = requests.get('http://localhost:4400/rest/2.0/assetTypes?name='+ assetTypeName + '&nameMatchMode=ANYWHERE', auth=('Admin', 'admin'))
-        print r.json()['total']
-        if(r.json()['total'] > 0):
-           return r.json()['results'][0]['id']
-        return ''
-    except ValueError:
-        return False
 
 def init():
     ##fileLoc = raw_input("Enter test script: ")
 
     fileObject = open('C:\Users\elliot\Documents\personal\workflow_dev_tools\\res\\nAT1.coltest', 'r')
 
+    dataManip = DataManip("http://localhost:4400")
+    
     for line in fileObject:
         line = line.strip()
         if(line == '\n'):
@@ -63,7 +69,7 @@ def init():
         if(splitBySpace[0] == 'create'):
             print(splitBySpace)
             if(splitBySpace[1] == 'asset'):
-                assetTypeId = findAssetType(splitBySpace[7])
+                assetTypeId = dataManip.findAssetType(splitBySpace[7])
                 
 
     fileObject.close()
