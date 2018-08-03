5a6973997f01cc23a7d017131eedf9157444fd93
elldi <elliotdines1@gmail.com>
2018-08-03 10:04:38 +0100

    Work done for saving an environment to a file. With testes along with it.

---
+++ src/main/java/coltools/DataStore.java
@@ -0,0 +1,7 @@
+package coltools;
+
+/**
+ * Created by elliot on 03/08/2018.
+ */
+public class DataStore {
+}
---
+++ src/test/java/coltools/EnvironmentControllerTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by elliot on 03/08/2018.
+ */
+public class EnvironmentControllerTest {
+
+}
